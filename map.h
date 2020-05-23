6890887e252208705c1a27e31bdb5a426a2ef2dc
BrianF10 <brian.angulo@phystech.edu>
2020-05-24 00:19:04 +0300

    add header map class

---
+++ map.h
@@ -0,0 +1,28 @@
+#ifndef MAP_H
+#define MAP_H
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+class Map
+{
+public:
+    Map();
+    void SetSize();
+    Map(const string& new_filename);
+    Map& operator = (const Map&);
+    ~Map();
+    int operator() (int x, int y);
+    int GetWidth() const;
+    int GetHeight() const;
+//    Map::Map& operator = (const Map&);
+private:
+    string filename;
+    ifstream myfile;
+    int width;
+    int height;
+    char* map;
+};
+
+#endif // MAP_H
